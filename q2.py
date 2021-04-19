a108264c3b990e2ce78646690b1d951e7357dbe5
Said Zeid <73799583+said-zeid@users.noreply.github.com>
2021-04-19 10:07:52 +0200

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,15 @@
+num= int(input("please enter num: "))
+"""
+print("sum equals to" + str(sum(range(1,num+1))))
+print("sum equals to" st)
+
+"""
+sums=0
+for x in range(1,num+1):
+    sums=sums+x
+avg=float(sums/num)
+print("sum equals to " +str(sums))
+print("avg equals to " +str(avg))
+
+
+
