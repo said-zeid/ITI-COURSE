6c8281b33b89adda9c4d09f6083090c58e9064d3
Said Zeid <73799583+said-zeid@users.noreply.github.com>
2021-05-04 23:56:04 +0200

    Add files via upload

---
+++ ros2_lab3/node3.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from example_interfaces.srv import SetBool
+from saidmsg.msg import New
+from saidmsg.srv import Firstone
+
+
+class my_server(Node):
+    def __init__(self):
+        super().__init__("node3")
+        self.client = self.create_client(Firstone, "First_server")
+        self.request = Firstone.Request()
+        self.service_client()
+
+    def service_client(self):
+        #client = self.create_client(SetBool, "First_server")
+        while self.client.wait_for_service(0.25) == False:
+            self.get_logger().warn("wating for server")
+
+        self.get_logger().info("client_request_sent")
+        self.request.f = 1
+        futur_obj = self.client.call_async(self.request)
+        futur_obj.add_done_callback(self.future_call)
+
+    def future_call(self, future_msg):
+        print("future msg")
+        # self.get_logger().info(str(future_msg.result().data))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node3 = my_server()
+    # node3.service_client()
+    rclpy.spin(node3)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
