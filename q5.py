3a25e8dbf1c430dac66ca4a718c6360d5f15539e
Said Zeid <73799583+said-zeid@users.noreply.github.com>
2021-05-18 09:50:30 +0200

    Delete q5.py

--- q5.py
+++
@@ -1,7 +0,0 @@
-s=input("enter any string to remove vowels ")
-#vowels=('a','e','u')
-for x in s.lower():
-    if x =='a' or x=='e' or x=='u' or x=='i' or x=='o':
-        s=s.replace(x,"")
-
-print(s) 
