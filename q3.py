642662bd8a2156ad702cc5e80d998e76deb88c5e
Said Zeid <73799583+said-zeid@users.noreply.github.com>
2021-05-18 09:52:52 +0200

    Delete q3.py

--- q3.py
+++
@@ -1,9 +0,0 @@
-rows = int(input("enter rows "))
-k = 2 * rows - 2
-for i in range(0, rows):
-    for j in range(0, k):
-        print(end=" ")
-    k = k - 1
-    for j in range(0, i + 1):
-        print("* ", end="")
-    print("")
